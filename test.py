bf70d5a5df1e63a32727d29d82489315339e1221
RakeshJogula <Rakeshjogula@gmail.com>
2022-08-25 01:33:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
 # Import os module
 import os
 
-# Iterate loop to read and print all environment variables
-print("The keys and values of all environment variables:")
-for key in os.environ:
-    print(key, '=>', os.environ[key])
-
-# Print the value of the particular environment variable
-print("The value of HOME is: ", os.environ['HOME'])
+vault_token = os.environ['token']
+print(vault_token)
