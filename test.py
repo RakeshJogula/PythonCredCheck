215ff0dcdaf2099136ea4a957dfc3080b2ef706a
RakeshJogula <Rakeshjogula@gmail.com>
2022-08-24 21:18:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-    user=os.getenv('DB_USERNAME'),
-    password=os.getenv('DB_PASSWORD'))
-    print(user)
-    print(password)
+import os
+
+user=os.getenv('DB_USERNAME')
+password=os.getenv('DB_PASSWORD')
+print(user)
+print(password)
