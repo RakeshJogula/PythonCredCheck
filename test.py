79a31a65569444286e5c48086cabd75e8e45c5ad
RakeshJogula <Rakeshjogula@gmail.com>
2022-08-25 03:44:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import os
 import base64
 
-encoded = base64.b64encode(b'os.getenv('DB_TOKEN')')
+encoded = base64.b64encode(os.getenv('DB_TOKEN'))
 print(os.getenv('DB_TOKEN'))
 print(encoded)
 b = base64.b64decode(encoded).decode("utf-8", "ignore")
