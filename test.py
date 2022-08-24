18d3155654528371b32fd546d2c3f04475e4cf94
RakeshJogula <Rakeshjogula@gmail.com>
2022-08-24 22:59:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import os
 import sys
+from time import sleep
+
 
 user=os.getenv('DB_USERNAME')
 password=os.getenv('DB_PASSWORD')
+time.sleep(3)
 print(user)
+time.sleep(3)
 print(password)
-sys.stdout.write(user)
-sys.stdout.write(password)
-sys.stdout.flush()
+time.sleep(3)
