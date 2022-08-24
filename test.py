7ac4d34938428d85639eac93bfcaa5222caf689f
RakeshJogula <Rakeshjogula@gmail.com>
2022-08-25 03:07:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 # Import os module
 import os
 
-username = os.environ['DB_USERNAME']
-password = os.environ['DB_PASSWORD']
-print(username)
-print(password)
-
+print(os.getenv('DB_USERNAME'))
+print(os.getenv('DB_PASSWORD'))
 
